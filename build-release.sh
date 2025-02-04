e196eddef4fc80c1a7bf59c1f1ae7665aefc2dcb
Kenneth Skovhede <kenneth@hexad.dk>
2025-02-04 22:02:11 +0100

    Created release builder script

---
+++ build-release.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Build the release version of the project
+RIDS="win-x64 linux-x64 osx-arm64"
+VERSION=1.0.0
+DATE=$(date +%Y-%m-%d)
+
+# Remove the existing release folder
+rm -rf ./bin/Release
+
+for RID in $RIDS
+do
+    # Build a release version of the project
+    dotnet publish -c Release -o ./bin/Release/$RID --self-contained -r $RID
+    
+    # Create a zip file for each platform
+    pushd ./bin/Release/$RID
+    zip -r ../../Release-$RID-$VERSION-$DATE.zip .
+    popd
+done
